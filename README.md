15ebc322bfd5993fcfc4876929a902ff72e97368
David Kwasny <dkwasny67@gmail.com>
2025-03-06 01:10:52 -0500

    Finalizing the inital scaffolding

---
+++ README.md
@@ -0,0 +1,12 @@
+Acore App
+=========
+
+[![Build](https://github.com/dkwasny/acore-app/actions/workflows/main.yml/badge.svg)](https://github.com/dkwasny/acore-app/actions/workflows/main.yml)
+
+A project for learning Spring Boot, React, Typescript, and other tech as I go along.
+
+Intended to be a fun little dashboard for the MySQL database backing the [AzerothCore](https://www.azerothcore.org/) WoW server emulator.
+
+### How to Run
+1. `gradlew bootRun`
+2. Hit `localhost:8080` in your web browser
--- acore-app-ui/build.gradle.kts
+++ acore-app-ui/build.gradle.kts
@@ -1,4 +1,37 @@
+/*
+ * A series of hardcoded tasks that delegate to NPM.
+ * I want the UI to be built with native tooling but still executed via
+ * stupid simple Gradle tasks.
+ */
+
+tasks.register<Exec>("npmInstall") {
+    inputs.files("package.json")
+    outputs.files("package-lock.json")
+    outputs.dirs("node_modules")
+
+    commandLine("npm", "install")
+}
+
+tasks.register<Exec>("npmBuild") {
+    dependsOn("npmInstall")
+
+    inputs.files(
+        "eslint.config.js",
+        "index.html",
+        "package-lock.json",
+        "tsconfig.app.json",
+        "tsconfig.json",
+        "tsconfig.node.json",
+        "vite.config.ts"
+    )
+    inputs.dir("node_modules")
+    inputs.dir("src")
+    inputs.dir("public")
+    outputs.dirs("dist")
+
+    commandLine("npm", "run", "build")
+}
 
 tasks.register("build") {
-    println("TOTALL BUILDING!!!!")
+    dependsOn("npmBuild")
 }
